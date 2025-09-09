bfc1aa93c7c7c55e30b0d36915a6b32768f8b8e8
Rafael Magno <rafaelmagno125@gmail.com>
2025-09-09 19:20:49 -0300

    fix: resolvendo merge de dashboard

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts"></script>
 
 <template>
-<<<<<<< HEAD
     <div>
         <input type="checkbox" id="menu-toggle">
         <section class="sidebar__wrapper">
@@ -90,12 +89,6 @@
             </main>
         </section>
     </div>
-=======
-  <div class="page-header">
-    <h1>Dashboard</h1>
-    <small>Home / Dashboard</small>
-  </div>
->>>>>>> 1f4f8ad448dedaaf8d39b609ce1eb8b498f018de
 </template>
 
 <style lang="sass" scoped>
